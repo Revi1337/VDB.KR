cdfd7f6fced600be66815bd924574bafa85eb26d
Revi1337 <david122123@gmail.com>
2023-06-27 18:49:29 +0900

    [Back : Initialize]
    * SpringSecurity 는 의존성을 추가하였지만, 초기개발단계에는 방해만되기때문에 잠시 주석처리
    
    * queryDsl 의존성 추가

---
+++ src/main/java/com/example/vdbkr/VdbKrApplication.java
@@ -0,0 +1,13 @@
+package com.example.vdbkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdbKrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VdbKrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vdbkr/VdbKrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vdbkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdbKrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
