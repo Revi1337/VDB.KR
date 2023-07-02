d515aec564ef9628617fc2e9fcae497be41978c0
Revi1337 <david122123@gmail.com>
2023-07-03 00:15:32 +0900

    [Back : UserAccount Entity 와 DTO 생성]

---
+++ src/main/java/com/revi1337/dto/UserAccountDto.java
@@ -0,0 +1,27 @@
+package com.revi1337.dto;
+
+import com.revi1337.domain.UserAccount;
+
+public record UserAccountDto(
+        Long id,
+        String email,
+        String username,
+        String password
+) {
+    public static UserAccountDto of(Long id,
+                                    String email,
+                                    String username,
+                                    String password) {
+        return new UserAccountDto(id, email, username, password);
+    }
+
+    public UserAccount toEntity() {
+        return UserAccount.create()
+                .id(id)
+                .email(email)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+}
