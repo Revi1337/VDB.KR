480678682e9015662e584be6942fc7aef1b3c545
Revi1337 <david122123@gmail.com>
2023-07-25 20:55:44 +0900

    [Frontend - MainLayout 을 구성하는 컴포넌트]

---
+++ frontend/src/components/ContentComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page-container class="row items-center justify-center">
+    <div :style="pageStyle">
+      <router-view />
+    </div>
+  </q-page-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const pageStyle = ref(['width: 1350px', 'height: 1500px']);
+</script>
