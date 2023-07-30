252973d777b61d3674bfe092e4ffab5cd9eaf8ff
Revi1337 <david122123@gmail.com>
2023-07-30 16:44:25 +0900

    [Back : git stash 로인해 생긴 중복 파일들 삭제]

--- src/main/java/com/revi1337/dto/response/nvd/Description.java
+++
@@ -1,7 +0,0 @@
-package com.revi1337.dto.response.nvd;
-
-public record Description(
-        String lang,
-        String value
-) {
-}
--- src/main/java/com/revi1337/dto/response/nvd/Vulnerabilities.java
+++
@@ -1,6 +0,0 @@
-package com.revi1337.dto.response.nvd;
-
-public record Vulnerabilities(
-        Cve cve
-) {
-}
