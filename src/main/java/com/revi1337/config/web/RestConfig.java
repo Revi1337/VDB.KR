be145a1bc39b829d99bb4a6816f7b112dbba7fa7
Revi1337 <david122123@gmail.com>
2023-07-31 15:41:15 +0900

    [Back: OpenEntityManagerConfig & RestConfig]

--- src/main/java/com/revi1337/config/web/RestConfig.java
+++ src/main/java/com/revi1337/config/web/RestConfig.java
@@ -1,5 +1,9 @@
 package com.revi1337.config.web;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +17,17 @@ public class RestConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
-                .setConnectTimeout(Duration.ofSeconds(17))
-                .setReadTimeout(Duration.ofSeconds(17))
+                .setConnectTimeout(Duration.ofSeconds(120))
+                .setReadTimeout(Duration.ofSeconds(120))
                 .build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
