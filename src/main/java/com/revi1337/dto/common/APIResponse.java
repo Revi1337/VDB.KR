17e911425ef2d72f427288dab0e843c4d1b2243a
Revi1337 <david122123@gmail.com>
2023-07-31 15:55:41 +0900

    [Back: 공통 Response, 및 ErrorResponse]

--- src/main/java/com/revi1337/dto/common/APIResponse.java
+++ src/main/java/com/revi1337/dto/common/APIResponse.java
@@ -2,6 +2,9 @@ package com.revi1337.dto.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
 public record APIResponse<T> (
         Header header,
@@ -9,23 +12,99 @@ public record APIResponse<T> (
         ErrorResponse error
 ) {
 
-    public static <T> APIResponse <T> of (Header header) {
-        return new APIResponse<>(header, null, null);
-    }
-
-    public static <T> APIResponse <T> of (T payload) {
-        return new APIResponse<>(null, payload, null);
-    }
-
     public static <T> APIResponse <T> of (ErrorResponse error) {
-        return new APIResponse<>(null, null, error);
+        return APIResponse.of(null, null, error);
     }
 
-    public static <T> APIResponse <T> of (Header header, T payload, ErrorResponse error) {
+    public static <T> APIResponse<T> of (T payload) {
+        return APIResponse.of(
+                Header.of(payload),
+                payload,
+                null
+        );
+    }
+
+    private static <T> APIResponse <T> of (Header header, T payload, ErrorResponse error) {
         return new APIResponse<>(header, payload, error);
     }
 
-    static class Header {}
+    @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+    record Header(
+            Integer statusCode,
+            Integer totalResults,
+            LocalDateTime timestamp
+    ) {
+        static Header of(Object object) {
+            if (object instanceof Collection<?>) {
+                return new Header(200, ((Collection<?>) object).size(), LocalDateTime.now());
+            }
+            return new Header(200, 1, LocalDateTime.now());
+        }
+    }
 
 }
+
+//import com.fasterxml.jackson.annotation.JsonInclude;
+
+//@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+//public record APIResponse<T> (
+//        T payload,
+//        ErrorResponse error
+//) {
+//
+//    public static <T> APIResponse <T> of (T payload) {
+//        return new APIResponse<>(payload, null);
+//    }
+//
+//    public static <T> APIResponse <T> of (ErrorResponse error) {
+//        return new APIResponse<>(null, error);
+//    }
+//
+//    public static <T> APIResponse <T> of (T payload, ErrorResponse error) {
+//        return new APIResponse<>(payload, error);
+//    }
+//
+//}
+
+
+//@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+//public record APIResponse<T> (
+//        Header header,
+//        T payload,
+//        ErrorResponse error
+//) {
+//
+//    public static <T> APIResponse<Collection<T>> of (Collection<T> payload) {
+//        return APIResponse.of(
+//                new Header(200, payload.size()),
+//                payload,
+//                null
+//        );
+//    }
+//
+//    public static <T> APIResponse<T> of (T payload) {
+//        return APIResponse.of(
+//                new Header(200, 1),
+//                payload,
+//                null
+//        );
+//    }
+//
+//    public static <T> APIResponse <T> of (ErrorResponse error) {
+//        return APIResponse.of(null, null, error);
+//    }
+//
+//    private static <T> APIResponse <T> of (Header header, T payload, ErrorResponse error) {
+//        return new APIResponse<>(header, payload, error);
+//    }
+//
+//    @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+//    record Header(
+//            Integer statusCode,
+//            Integer totalResults
+////            LocalDateTime timestamp
+//    ) {
+//    }
+//}
+
 
--- src/main/java/com/revi1337/dto/common/ErrorResponse.java
+++ src/main/java/com/revi1337/dto/common/ErrorResponse.java
@@ -1,21 +1,49 @@
 package com.revi1337.dto.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public record ErrorResponse (
         int statusCode,
         String message,
-        Map<String, String> validation
+        Map<String, String> validation,
+        LocalDateTime timestamp
 ) {
 
-    public static  ErrorResponse of(int statusCode, String message, Map<String, String> validation) {
-        return new ErrorResponse(statusCode, message, validation);
+    public static ErrorResponse of(int statusCode, LocalDateTime timestamp, Map<String, String> validation, String message) {
+        return new ErrorResponse(statusCode, message, validation, timestamp);
     }
 
-    public static  ErrorResponse of(int statusCode, String message) {
-        return new ErrorResponse(statusCode, message, new HashMap<>());
+    public static ErrorResponse of(int statusCode, String message, Map<String, String> validation) {
+        return ErrorResponse.of(statusCode, LocalDateTime.now(), validation, message);
+    }
+
+    public static ErrorResponse of(int statusCode, String message) {
+        return ErrorResponse.of(statusCode, message, new HashMap<>());
     }
 
 }
+
+
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public record ErrorResponse (
+//        int statusCode,
+//        String message,
+//        Map<String, String> validation
+//) {
+//
+//    public static ErrorResponse of(int statusCode, String message, Map<String, String> validation) {
+//        return new ErrorResponse(statusCode, message, validation);
+//    }
+//
+//    public static ErrorResponse of(int statusCode, String message) {
+//        return ErrorResponse.of(statusCode, message, new HashMap<>());
+//    }
+//
+//}
 
