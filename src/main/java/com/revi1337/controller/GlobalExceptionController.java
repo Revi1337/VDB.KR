5ec4ce733fc11200838394a98711c6e04f3f259f
Revi1337 <david122123@gmail.com>
2023-07-31 16:06:28 +0900

    [Back: 전역에서 사용되는 ExceptionHandler]
    Security 단을 포함해서 계층에맞게 익셉션을 분리시켜주었음.

--- src/main/java/com/revi1337/controller/GlobalExceptionController.java
+++ src/main/java/com/revi1337/controller/GlobalExceptionController.java
@@ -2,10 +2,14 @@ package com.revi1337.controller;
 
 import com.revi1337.dto.common.APIResponse;
 import com.revi1337.dto.common.ErrorResponse;
+import com.revi1337.exception.CustomJwtException;
 import com.revi1337.exception.RootException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -73,7 +77,61 @@ public class GlobalExceptionController {
     public ResponseEntity<?> rootExceptionHandler(RootException exception) {
         ErrorResponse errorResponse = ErrorResponse.of(
                 exception.getStatusCode(), exception.getExceptionMessage());
-        APIResponse<Object> apiResponse = APIResponse.of(errorResponse);
+        APIResponse<?> apiResponse = APIResponse.of(errorResponse);
+        return ResponseEntity
+                .status(errorResponse.statusCode())
+                .body(apiResponse);
+    }
+
+    /**
+     * Handling Security layer AuthenticationException occur by AuthenticationFailureHandler
+     * @AuthenticationFailureHandler executed when Authenticate failed. It throws
+     * {@link UsernameNotFoundException}, {@link BadCredentialsException}
+     * @param exception
+     */
+    @ExceptionHandler(value = {UsernameNotFoundException.class, BadCredentialsException.class})
+    public ResponseEntity<?> loginAuthenticationExceptionHandler(Exception exception) {
+        ErrorResponse errorResponse = ErrorResponse.of(401, "id or password incorrect");
+        APIResponse<?> apiResponse = APIResponse.of(errorResponse);
+        return ResponseEntity
+                .status(errorResponse.statusCode())
+                .body(apiResponse);
+    }
+
+    /**
+     * Handling Security layer AuthenticationException occur by AuthenticationEntryPoint
+     * @AuthenticationEntryPoint executed when non authenticated user or insufficient privileges user request endpoint  It throws
+     * {@link AuthenticationException}
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(value = AuthenticationException.class)
+    public ResponseEntity<?> authenticationEntryPointExceptionHandler(Exception exception) {
+        String exceptionName = exception.getClass().getSimpleName();
+        ErrorResponse errorResponse;
+        switch (exceptionName) {
+            case "DisabledException" ->
+                    errorResponse = ErrorResponse.of(403, "account disabled. plz activate email verification");
+            case "InsufficientAuthenticationException" ->
+                    errorResponse = ErrorResponse.of(403, "this endpoint must be called by authenticated user");
+            default -> errorResponse = ErrorResponse.of(403, "authenticated error");
+        }
+        APIResponse<?> apiResponse = APIResponse.of(errorResponse);
+        return ResponseEntity
+                .status(errorResponse.statusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = CustomJwtException.class)
+    public ResponseEntity<?> customJwtException(CustomJwtException exception) {
+        String exceptionName = exception.getJwtException().getClass().getSimpleName();
+        ErrorResponse errorResponse;
+        switch (exceptionName) {
+            case "ExpiredJwtException" -> errorResponse = ErrorResponse.of(401, "token expired");
+            case "SignatureException" -> errorResponse = ErrorResponse.of(401, "signature valid failed");
+            default -> errorResponse = ErrorResponse.of(401, "token error");
+        }
+        APIResponse<?> apiResponse = APIResponse.of(errorResponse);
         return ResponseEntity
                 .status(errorResponse.statusCode())
                 .body(apiResponse);
